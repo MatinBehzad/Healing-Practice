472d86ddbdbba03eb9366b3a42ec9c235731d6bd
matin <behz0003@algonquinlive.com>
2021-02-17 16:20:13 -0500

    Matineh First Commit

---
+++ src/components/NavButtons.tsx
@@ -0,0 +1,35 @@
+import { IonButton, IonMenuButton } from "@ionic/react";
+import React, { useEffect } from "react";
+
+export const NavButtons = () => {
+  const [mQuery, setMQuery] = React.useState<any>({
+    matches: window.innerWidth > 768 ? true : false,
+  });
+
+  useEffect(() => {
+    let mediaQuery = window.matchMedia("(min-width: 768px)");
+    mediaQuery.addListener(setMQuery);
+
+    return () => mediaQuery.removeListener(setMQuery);
+  }, []);
+
+  // MediaQueryListEvent { isTrusted: true, media: "(min-width: 768px)", matches: true ...}
+  console.log(mQuery.matches);
+
+  return (
+    <div>
+      {mQuery && !mQuery.matches ? (
+        <IonMenuButton />
+      ) : (
+        <>
+          <IonButton routerLink={"/home"}>Meet Lynda</IonButton>
+          <IonButton routerLink={"/page-1"}>Services</IonButton>
+          <IonButton routerLink={"/page-2"}>Rates and Packages</IonButton>
+          <IonButton routerLink={"/page-3"}>Contact Lynda</IonButton>
+          <IonButton routerLink={"/page-4"}>Testimonials</IonButton>
+        
+        </>
+      )}
+    </div>
+  );
+};
